fc0e5304c5ed8f1663f38ccbd0f585af776311cc
Petrik mpp <petr_mpp@yahoo.com>
2013-05-01 22:39:28 -0400

    GasTracker: markup PurchaseDetails page
    will use three_drop_down_lists approach on it

---
+++ Web/WebAppProject/ThreeDropListCalendar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI.WebControls;
+
+namespace WebAppProject
+{
+    public class ThreeDropListCalendar
+    {
+        private DropDownList _monthDropDownList;
+        private DropDownList _dayDropDownList;
+        private DropDownList _yearDropDownList;
+
+        public ThreeDropListCalendar(DropDownList monthDropDownList, DropDownList dayDropDownList, DropDownList yearDropDownList)
+        {
+            Validator.ThrowIfNull<ArgumentNullException>(monthDropDownList, "monthDropDownList");
+            Validator.ThrowIfNull<ArgumentNullException>(dayDropDownList, "dayDropDownList");
+            Validator.ThrowIfNull<ArgumentNullException>(yearDropDownList, "yearDropDownList");
+
+            _monthDropDownList = monthDropDownList;
+            _dayDropDownList = dayDropDownList;
+            _yearDropDownList = dayDropDownList;
+        }
+    }
+}
