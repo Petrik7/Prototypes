bfc2d02bedb773b46782e3ab9e1afea7cdd3b15f
Petrik mpp <petr_mpp@yahoo.com>
2016-03-25 19:55:14 -0400

    Shuffle
    Shuffle

---
+++ Shuffle/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+
+namespace Shuffle
+{
+    class Program
+    {
+        static void TestShuffle()
+        {
+            ShuffleNumbers shuffler = new ShuffleNumbers();
+            const int n = 8;
+            int[] res1 = shuffler.Shuffle(n);
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            int[] res2 = shuffler.Shuffle(n);
+
+            System.Diagnostics.Debug.Assert(n == res1.Length);
+            System.Diagnostics.Debug.Assert(n == res2.Length);
+
+            int diff = 0;
+            for (int i = 0; i < res1.Length; ++i)
+            {
+                if (res1[i] != res2[i])
+                    ++diff;
+            }
+            System.Diagnostics.Debug.Assert(diff >= n/2);
+
+            Array.Sort(res1);
+            for (int i = 0; i < res1.Length; ++i)
+            {
+                System.Diagnostics.Debug.Assert(res1[i] == (i + 1));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //TestShuffle();
+
+            int n = 0;
+            string nStr = string.Empty;
+            bool commandLineParam = (args.Length > 0);
+            ShuffleNumbers shuffler = new ShuffleNumbers();
+
+            if (!commandLineParam)
+            {
+                Console.Error.WriteLine("In interactive mode just type N below or start it with N as a command line parameter, Enjoy!");
+            }
+
+            do
+            {
+                if(commandLineParam)
+                {
+                    nStr = args[0];
+                }
+                else
+                {
+                    Console.WriteLine("Please enter integer N");
+                    nStr = Console.ReadLine();
+                }
+
+                if (int.TryParse(nStr, out n))
+                {
+                    try
+                    {
+                        OutputNumbers(shuffler.Shuffle(n));
+                    }
+                    catch (OutOfMemoryException)
+                    {
+                        Console.Error.WriteLine("Not enough memory to handle your request");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Error.WriteLine(ex.Message);
+                    }
+                }
+                else
+                {
+                    Console.Error.WriteLine("Input is not an integer.");
+                }
+                
+            }while (!commandLineParam);
+        }
+
+        static private void OutputNumbers(int[] numbers)
+        {
+            StringBuilder result = new StringBuilder();
+            foreach (int number in numbers)
+            {
+                result.Append(number);
+                result.Append(Environment.NewLine);
+            }
+            Console.Write(result.ToString());
+        }
+    }
+}
---
+++ Shuffle/ShuffleNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+
+namespace Shuffle
+{
+    class ShuffleNumbers
+    {
+        public int[] Shuffle(int n)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            int[] numbers = new int[n];
+                        
+            for (int i = 0; i < n; ++i)
+            {
+                numbers[i] = i + 1;    
+            }
+
+            Random randomGen = new Random();
+            for (int i = 0; i < n - 1; ++i)
+            {
+                int j = randomGen.Next(i, n);
+                //swap intentionally inlined
+                int tmp = numbers[j];
+                numbers[j] = numbers[i];
+                numbers[i] = tmp;
+            }
+
+            stopWatch.Stop();
+            Console.Error.WriteLine("Shuffling of {0} numbers took {1} ms",  n, stopWatch.Elapsed.TotalMilliseconds);
+            return numbers;
+        }
+    }
+}
